1e1db34ce20c3fe8362edf6ed2c8c0ebd585b833
Drajeske, Matthew (S&T-Student) <mpd4vb@mst.edu>
2019-09-12 21:26:55 -0500

    Working version that meets output standards

--- fishbomb.cpp
+++ fishbomb.cpp
@@ -11,22 +11,23 @@
 using namespace std;
 
 void getDimensions(int&, int&);
-void allocateFish(int, int);
-void bombFish(int*[], int, int, int, int);
+void allocateFish(int&, int, int);
+int bombFish(int*[], int, int, int, int);
 
 int main() {
 
    //rows and columns represent the dimensions of the fish matrix
    //x and y represent the coordinates of the dynamite
    //numTrials is the number of trials 
-   int rows, columns, numTrials;
+   int rows, columns;
+   int numTrials;
 
    cin >> numTrials;
 
    //execute functions for however many trials the user inputs
    for (int i = 0; i < numTrials; i++) {
       getDimensions(rows, columns);
-      allocateFish(rows, columns);
+      allocateFish(i, rows, columns);
    }
    return 0;
 }
@@ -35,7 +36,7 @@ void getDimensions(int& r, int& c) {
    cin >> r >> c;
 };
 
-void allocateFish(int rows, int columns) {
+void allocateFish(int& trialNum, int rows, int columns) {
 
    //columns in 2D array of fish
    int** fishArray = new int*[rows];
@@ -52,29 +53,33 @@ void allocateFish(int rows, int columns) {
       }
    }
 
-   //test 1, default array with no changes
+   /*//test 1, default array with no changes
    for (int i = 0; i < rows; i++) {
       for (int j = 0; j < columns; j++) {
          cout << fishArray[i][j]; //works without a space but a space breaks it for some reason (?)
                                   //seems to be an issue with the test idk it's weird
       }
       cout << endl;
-   }
+   }*/
 
    //3 sticks of dynamite are thrown. These will be the coordinates they are thrown to.
    int x1, y1;
    int x2, y2;
    int x3, y3;
 
-   cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3; 
+   cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+   //counter for having correct output format
+
 
    //function call
-   bombFish(fishArray, x1, y1, rows, columns);
-   bombFish(fishArray, x2, y2, rows, columns);
-   bombFish(fishArray, x3, y3, rows, columns);
+   cout << "#" << trialNum << ": Bender catches " <<
+      bombFish(fishArray, x1, y1, rows, columns) + bombFish(fishArray, x2, y2, rows, columns) + bombFish(fishArray, x3, y3, rows, columns)
+      << " fish" << endl;
+
 };
 
-void bombFish(int* fishArray[], int x, int y, int rows, int columns) {
+int bombFish(int* fishArray[], int x, int y, int rows, int columns) {
 
    //Bombing a fish means setting its (x, y) coordinate in the grid to 0 and add its value to total fish caught
     //integers to mark where in the array the fish were bombed. (startX, startY) is top left corner of blast,
@@ -131,7 +136,7 @@ void bombFish(int* fishArray[], int x, int y, int rows, int columns) {
       }
    }
 
-   //test 2, array after it is bombed
+   /*//test 2, array after it is bombed
    for (int i = 0; i < rows; i++) {
       for (int j = 0; j < columns; j++) {
          cout << fishArray[i][j]; //works without a space but a space breaks it for some reason (?)
@@ -139,8 +144,6 @@ void bombFish(int* fishArray[], int x, int y, int rows, int columns) {
       }
       cout << endl;
    }
-   cout << endl;
+   cout << endl;*/
+   return fishCaught;
 };
-
-
-
